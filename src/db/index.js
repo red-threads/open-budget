4e60697d481c0b71a003c80c04cf6e18c2e079a2
jsDotCr <rocco@js.cr>
2018-06-17 16:07:56 +0200

    chore: use rollbar for db failures

--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose')
+const rollbar = require('../rollbar')
 
 const url = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_SERVER}/test?retryWrites=true`
 
 module.exports = function () {
   mongoose.connect(url)
   mongoose.connection.on('error', (err) =>
-    console.error('connection error', err)
+    rollbar.error(err)
   )
   mongoose.connection.once('open', function () {
     console.info('Connection succeeded')
