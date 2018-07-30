f60b5fb37cff37395716ad47e015f3ff9e50f84d
Rocco Curcio <rocco@js.cr>
2018-07-30 11:25:04 +0200

    fix: variable mongo protocol

--- src/db/index.js
+++ src/db/index.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose')
 const rollbar = require('../rollbar')
 
-const url = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_SERVER}/test?retryWrites=true`
+const url = `${process.env.MONGO_PROTOCOL}://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_SERVER}/${process.env.MONGO_DB}?retryWrites=true`
 
 module.exports = function () {
+  console.info('will connect to', process.env.MONGO_SERVER)
   mongoose.connect(url)
-  mongoose.connection.on('error', (err) =>
+  mongoose.connection.on('error', (err) => {
+    console.error('Connection failed')
+    console.error(err)
     rollbar.error(err)
-  )
+  })
   mongoose.connection.once('open', function () {
     console.info('Connection succeeded')
   })
